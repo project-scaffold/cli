86bd6a000368d101b009267bc828b2e1f65666f0
Jiangtao <321jiangtao@gmail.com>
2018-02-04 22:56:22 +0800

    update

--- interfaces/hook.js
+++ interfaces/hook.js
@@ -2,6 +2,7 @@ const execSync = require('child_process').execSync
 
 module.exports = {
   after(answer, reply){
+    console.log('hook after starting')
     execSync(`cd ${answer.scaffold} && npm i`)
     console.log('hook after done')
   }
